accc85a8e0d2bdc9afacb7450547248da3b247a0
flixofarrell <flix.ofarrell@gmail.com>
2020-11-12 17:59:16 +0000

    use tidyverse

--- r_dependencies.r
+++ r_dependencies.r
@@ -1,11 +1,7 @@
 install.packages("data.table", repos = "http://cran.us.r-project.org")
 
-install.packages("readr", repos = "http://cran.us.r-project.org")
-
-install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+install.packages("tidyverse", repos = "http://cran.us.r-project.org"), 
 
 install.packages("ggrepel", repos = "http://cran.us.r-project.org")
-
-install.packages("dplyr", repos = "http://cran.us.r-project.org")
 
 install.packages("RColorBrewer", repos = "http://cran.us.r-project.org")
