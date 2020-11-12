988e0014a3b3d527c0f5cfd5806b7a5dbc4a8a30
flixofarrell <flix.ofarrell@gmail.com>
2020-11-12 17:38:36 +0000

    new approach

---
+++ r_dependencies.r
@@ -0,0 +1,11 @@
+install.packages("data.table")
+
+install.packages("readr")
+
+install.packages("ggplot2")
+
+install.packages("ggrepel")
+
+install.packages("dplyr")
+
+install.packages("RColorBrewer")
